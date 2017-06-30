8e605ce5196a5263ea8601989d850b3b256f05e3
Tobynson <mikatu1@yahoo.com>
2017-06-30 00:23:40 -0500

    ejemplo de encapsulamineto

---
+++ getset/src/getset/Getset.java
@@ -0,0 +1,11 @@
+
+package getset;
+
+public class Getset {
+
+    public static void main(String[] args) {
+        Punto pt = new Punto(4,6);
+        System.out.println(pt.toString());
+    }
+    
+}
